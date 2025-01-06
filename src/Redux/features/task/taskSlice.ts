ba93d9693dfbecb1651534f460f7b0155dcd6df3
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-01-06 18:53:17 +0600

    add: task type

--- src/Redux/features/task/taskSlice.ts
+++ src/Redux/features/task/taskSlice.ts
@@ -1,8 +1,22 @@
+import { ITask } from "@/type";
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-
+interface IInitialState {
+    task:ITask[]
 }
+
+const initialState :IInitialState = {
+  task: [
+    {
+      id: "adsfasdf",
+      title: "Initialize frontend",
+      description: "create home page and routing",
+      dueDate: "2025-11",
+      isCompleted: false,
+      priority: "high",
+    },
+  ],
+};
 
 const todoSlice = createSlice({
     name:'todo',
@@ -11,3 +25,6 @@ const todoSlice = createSlice({
 
     }
 })
+
+
+export default todoSlice.reducer
