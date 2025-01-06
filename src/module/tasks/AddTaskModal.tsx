d701b9cf4ae5d47834d4b6bd5efe6dcb9eeec1cd
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-01-06 20:34:50 +0600

    add: select option

--- src/module/tasks/AddTaskModal.tsx
+++ src/module/tasks/AddTaskModal.tsx
@@ -8,6 +8,8 @@ import {
 } from "@/components/ui/dialog";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
 import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
 import { useForm } from "react-hook-form";
 
@@ -22,34 +24,92 @@ export function AddTaskModal() {
         <Button variant="outline">Add Task</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
-        <DialogDescription className="sr-only">Fill up this form to add task</DialogDescription>
+        <DialogDescription className="sr-only">
+          Fill up this form to add task
+        </DialogDescription>
         <DialogHeader>
-            <DialogTitle>
-                Add Task
-            </DialogTitle>
+          <DialogTitle>Add Task</DialogTitle>
         </DialogHeader>
-       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onsubmit)}>
-        <FormField
-            control={form.control}
-            name="title"
-            render={({field}) => (
+        <Form {...form}>
+          <form className="space-y-3" onSubmit={form.handleSubmit(onsubmit)}>
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
                 <FormItem>
-                <FormLabel />
-                <FormControl>
-                <Input {...field}/>
-                </FormControl>
-                <FormDescription />
-                <FormMessage />
-            </FormItem>
-            )}
-        />
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-        </form>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input {...field} value={field.value || ""} />
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} value={field.value || ""} />
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="dueDate"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Due Date</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} value={field.value || ""} />
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="priority"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Priority</FormLabel>
+                  <FormControl>
+                    <Select 
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    >
+                      <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Select a fruit" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Fruits</SelectLabel>
+                          <SelectItem value="apple">Apple</SelectItem>
+                          <SelectItem value="banana">Banana</SelectItem>
+                          <SelectItem value="blueberry">Blueberry</SelectItem>
+                          <SelectItem value="grapes">Grapes</SelectItem>
+                          <SelectItem value="pineapple">Pineapple</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </form>
         </Form>
-
       </DialogContent>
     </Dialog>
   );
